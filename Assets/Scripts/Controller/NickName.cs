01897f30b86f08f529700651cfdca46ff3107a30
jsy-00 <jsy00@sookmyung.ac.kr>
2023-06-17 12:29:59 +0900

    닉네임 안 뜨는거 해결

---
+++ Assets/Scripts/Controller/NickName.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NickName : MonoBehaviour
+{
+    [SerializeField] Text playerName_TEXT;
+
+    #region Private variable
+    private Canvas canvas;
+    private PhotonView pv;
+
+    private Player player;
+    #endregion
+
+    #region LifeCycle
+    private void Awake()
+    {
+        pv = this.gameObject.GetComponentInParent<PhotonView>();
+        canvas = this.GetComponent<Canvas>();
+    }
+
+    private void Start()
+    {
+        playerName_TEXT.text = pv.Owner.NickName;
+        // 해당 Player가 방에 입장하거나 생성할 때 입력한 닉네임을 불러와 출력
+    }
+
+    #endregion
+}
